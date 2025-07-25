9e9d7f43f870ab22d67f4b129bcfa5d971a3978c
phkmolina1986 <pedro.kuchak@gmail.com>
2025-07-25 12:10:01 -0600

    main call preparation

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,9 +1,16 @@
+//function to clear the body
+function ClearBody(){
+    console.log(document.getElementsByTagName("body")[0].innerHTML = "");
+}
+
 //add actions for sign in and get started button with JQuery
-$(() => {
-    $('#signInButton').on('click', () => {  
+$(() => {        
+    $('#signInButton').on('click', () => {          
+        ClearBody()
         Login();
     }); 
-    $('#getStartedButton').on('click', () => {
+    $('#getStartedButton').on('click', () => {        
+        ClearBody()
         Registration();
     }
     );
