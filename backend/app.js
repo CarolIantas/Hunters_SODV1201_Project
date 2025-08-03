4ae6793b5b4297662dd5ed1925ad5b1714904f0d
Tantand24 <tristan.dang@email.com>
2025-08-03 17:31:36 -0600

    more setup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const path = require('path')
-const fs = require('fs')
+const cors = require('cors')
 const express = require('express')
 
 
 const app = express()
+const PORT = process.env.PORT
+
+app.use(cor())
+app.use(express.json())
 console.log("Backend login here")
