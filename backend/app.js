651677f682d57d673055c03fbee9e0b4fb09b09a
Tantand24 <tristan.dang@email.com>
2025-08-05 09:35:02 -0600

    changes

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,19 @@ const express = require('express')
 
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+const user = require('./routing/user')
+const properties = require('./routing/properties')
+const workspace = require('./routing/workspaces')
 console.log("Backend login here")
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
