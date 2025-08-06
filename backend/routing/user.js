de13a7695647d593f784b1abeebc236bd4e9ce22
Tantand24 <tristan.dang@email.com>
2025-08-06 17:58:42 -0600

    formatting data

--- backend/routing/user.js
+++ backend/routing/user.js
@@ -35,7 +35,19 @@ router.post('/users', (req, res) => {
     return res.status(409).json({ message: "User with this Email already exists" });
   }
 
-  users.push(newUser);
+  // Formatting data save to database
+  const SavingUser = {
+    id: users.length+1,
+    Fullname: `${newUser.firstName} ${newUser.lastName}`,
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    email: newUser.email,
+    phone: newUser.phone,
+    password: newUser.password,
+    role: newUser.role
+  };
+
+  users.push(SavingUser);
   writeUsers(users);
 
   res.status(201).json({ message: "User created", user: newUser });
