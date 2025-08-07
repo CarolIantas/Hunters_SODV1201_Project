025d1156c18e2c890115be333c44b8a2a2d112a0
phkmolina1986 <pedro.kuchak@gmail.com>
2025-08-07 11:22:01 -0600

    Update console from back server

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,6 @@ app.use(workspaceRoutes)
 //Port and listening for server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    
+     console.log(`Server is running on http://localhost:${PORT}`)
 })
 
